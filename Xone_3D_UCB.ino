82e67eff8b8187e433c6e18d6133afc8a820d701
Rutger Willems <rut.willems@gmail.com>
2021-10-26 23:42:53 +0200

    get to fully working toggle state

--- Xone_3D_UCB.ino
+++ Xone_3D_UCB.ino
@@ -1,17 +1,5 @@
 #include <Wire.h>
 
-#define DEBOUNCE 4
-#define DMASK ((1<<DEBOUNCE)-1)
-#define DF (1<<(DEBOUNCE-1))
-#define DR (DMASK-DF)
-
-//macro for detection of raising edge and debouncing
-#define DRE(signal, state) ((state=((state<<1)|(signal&1))&DMASK)==DR)
-
-//macro for detection of falling edge and debouncing
-#define DFE(signal, state) ((state=((state<<1)|(signal&1))&DMASK)==DF)
-
-
 //Address of MCP23016 IO Expander, 8 addresses available
 #define IO_L        B00100011
 #define IO_R        B00100111
@@ -38,15 +26,15 @@
 #define L1_O_CUE_DC_2  6
 
 
-#define R0_I_LP2_SEL 4
-#define R0_I_ON2_SEL 5
-#define R0_I_HP2_SEL 6
-#define R0_I_BP2_SEL 7
+#define R0_I_LP2_SEL   4
+#define R0_I_ON2_SEL   5
+#define R0_I_HP2_SEL   6
+#define R0_I_BP2_SEL   7
 
-#define R0_O_LP2_DC 0
-#define R0_O_ON2_DC 1
-#define R0_O_HP2_DC 2
-#define R0_O_BP2_DC 3
+#define R0_O_LP2_DC    0
+#define R0_O_ON2_DC    1
+#define R0_O_HP2_DC    2
+#define R0_O_BP2_DC    3
 
 
 #define R1_I_CUE_SND_3 2
@@ -102,12 +90,12 @@ byte R1 = B00000000;
 int cue[5] = {0, 0, 0, 0, 0};
 int filter1[4] = {0, 0, 0, 0};
 int filter2[4] = {0, 0, 0, 0};
+
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
 void setup(void) {
   pinMode(interruptPin, INPUT);
 
   Serial.begin(9600);  // start serial for output
-  Serial.println("Starting up");
 
   
   Wire.begin();          // join i2c bus (address optional for master)
@@ -137,12 +125,13 @@ void setup(void) {
   // write_io(IO_L, IO_gp00, B11111111, B11111000); // same as below
   write_io(IO_L, IO_gp00, B01010110, B01100000);
 
-  write_io(IO_R, IO_gp00, B11111111, B11111111); // test
+  write_io(IO_R, IO_gp00, B11111111, B11111111);
+  
   delay(500);
 
   // all LED's off
-  write_io(IO_L, IO_gp00, B00000000, B00000111);  
-  write_io(IO_R, IO_gp00, B00000000, B00000000); // test
+  write_io(IO_L, IO_gp00, L1, L0);  
+  write_io(IO_R, IO_gp00, R1, R0);
 }
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -196,16 +185,10 @@ void on_interupt () {
   byte INTCAP1R = read_io(IO_R, INTCAP1);
   delay(1);
 
-//  Serial.println(INTCAP0L);
-//  Serial.println(INTCAP1L);
-//  Serial.println(INTCAP0R);
-//  Serial.println(INTCAP1R);
 
   proccess_inputs(INTCAP0L, INTCAP1L, INTCAP0R, INTCAP1R);
 
   write_outputs();
-
-  // delay(10); // POOR MANS DEBVOUNCE
 }
 
 
@@ -225,12 +208,16 @@ void write_outputs () {
   
   bitWrite(out_r_1, R1_O_CUE_DC, cue[4]); // CUE_DC
 
-  // DEMO DOES NOT WORK LIKE IT SHOULD DOES A WIERD TING on purposse ( setting the filer mode with the on)
-  bitWrite(out_l_1, L1_O_BP1_DC, filter1[0]); // BP1_DC
+  // Does not work like upposed to, toggles modes individuall, also mayby set BP or HP by defaulrt
+  bitWrite(out_l_0, L0_O_LP1_DC, filter1[1]);
+  bitWrite(out_l_1, L1_O_BP1_DC, filter1[2]); // BP1_DC
+  bitWrite(out_l_0, L0_O_HP1_DC, filter1[3]);
   bitWrite(out_l_1, L1_O_ON1_DC, filter1[0]); // ON1_DC
 
-  // DEMO DOES NOT WORK LIKE IT SHOULD DOES A WIERD TING on purposse ( setting the filer mode with the on)
-  bitWrite(out_r_0, R0_O_HP2_DC, filter2[0]); // BP1_DC
+  // Does not work like upposed to, toggles modes individuall, also mayby set BP or HP by defaulrt
+  bitWrite(out_r_0, R0_O_LP2_DC, filter2[1]);
+  bitWrite(out_r_0, R0_O_BP2_DC, filter2[2]); // BP1_DC
+  bitWrite(out_r_0, R0_O_HP2_DC, filter2[3]);
   bitWrite(out_r_0, R0_O_ON2_DC, filter2[0]); // ON1_DC
   
 
@@ -251,26 +238,32 @@ void read_inputs () {
   proccess_inputs(GP0L, GP1L, GP0R, GP1R);
 }
 
-bool bitComp(byte b0, byte b1, int i) {
-  return bitRead(b0, i) != bitRead(b1, i);
-}
+
+// bit comparison rising
+#define BCR(B0, B1, I) (!bitRead(B0, I) && bitRead(B1, I))
 
 
 void proccess_inputs (byte GP0L, byte GP1L, byte GP0R, byte GP1R) {
-
+  
   // toggle the cue's on and off (no debounce)
-  cue[0] = bitComp(L1, GP1L, L1_I_CUE_SND_1) ? !cue[0]: cue[0]; // CUE_SND_1
-  cue[1] = bitComp(L1, GP1L, L1_I_CUE_SND_2) ? !cue[1]: cue[1]; // CUE_SND_2
-  cue[2] = bitComp(R1, GP1R, R1_I_CUE_SND_3) ? !cue[2]: cue[2]; // CUE_SND_1
-  cue[3] = bitComp(R1, GP1R, R1_I_CUE_SND_4) ? !cue[3]: cue[3]; // CUE_SND_2
+  cue[0] = BCR(L1, GP1L, L1_I_CUE_SND_1) ? !cue[0]: cue[0]; // CUE_SND_1
+  cue[1] = BCR(L1, GP1L, L1_I_CUE_SND_2) ? !cue[1]: cue[1]; // CUE_SND_2
+  cue[2] = BCR(R1, GP1R, R1_I_CUE_SND_3) ? !cue[2]: cue[2]; // CUE_SND_1
+  cue[3] = BCR(R1, GP1R, R1_I_CUE_SND_4) ? !cue[3]: cue[3]; // CUE_SND_2
 
+  // if any of the cue on turn cue router on
   cue[4] = cue[0] || cue[1] || cue[2] || cue[3];
 
 
-  // these two filters currently follow a janky toggle no select flow
-  filter1[0] =  bitComp(L1, GP1L, L1_I_ON1_SEL) ? !filter1[0]: filter1[0]; // ON1_SEL
+  filter1[0] =  BCR(L1, GP1L, L1_I_ON1_SEL) ? !filter1[0]: filter1[0]; // ON1_SEL
+  filter1[1] =  BCR(L0, GP0L, L0_I_LP1_SEL) ? !filter1[1]: filter1[1];
+  filter1[2] =  BCR(L0, GP0L, L0_I_BP1_SEL) ? !filter1[2]: filter1[2];
+  filter1[3] =  BCR(L0, GP0L, L0_I_HP1_SEL) ? !filter1[3]: filter1[3];
   
-  filter2[0] =  bitComp(R0, GP0R, R0_I_ON2_SEL) ? !filter2[0]: filter2[0]; // ON1_SEL
+  filter2[0] =  BCR(R0, GP0R, R0_I_ON2_SEL) ? !filter2[0]: filter2[0]; // ON2_SEL
+  filter2[1] =  BCR(R0, GP0R, R0_I_LP2_SEL) ? !filter2[1]: filter2[1];
+  filter2[2] =  BCR(R0, GP0R, R0_I_BP2_SEL) ? !filter2[2]: filter2[2];
+  filter2[3] =  BCR(R0, GP0R, R0_I_HP2_SEL) ? !filter2[3]: filter2[3];
 
   // update state
   L0 = GP0L;
